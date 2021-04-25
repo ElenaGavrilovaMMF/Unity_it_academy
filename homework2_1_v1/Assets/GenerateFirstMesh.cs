12fe516a4c1d3b85e4f8e35a0171c1c2a1a5cfed
LGavrik <lena.gavrilova2410@gmail.com>
2021-04-26 01:15:39 +0300

    [2-1]

---
+++ homework2_1_v1/Assets/GenerateFirstMesh.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class GenerateFirstMesh : MonoBehaviour
+{
+    public GameObject prefabMesh;
+    
+    private GameObject currentGameObject = null;
+    private float speed = 4f;
+    private bool movingFlag = true;
+    private float hideForGenarationMesh = 3f;
+    
+    void Start()
+    {
+        GameObject basedMeshObject = new GameObject();
+        basedMeshObject.AddComponent<MeshFilter>();
+        basedMeshObject.AddComponent<MeshRenderer>().material.color =  Color.blue;
+        Mesh basedMesh = new Mesh();
+        basedMeshObject.GetComponent<MeshFilter>().mesh = basedMesh;
+        basedMesh.vertices = GenerateVertices(10f , 10f);
+        basedMesh.triangles = GenerateTriangle();
+        basedMesh.RecalculateNormals();
+        MeshCollider collider = basedMeshObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+    }
+    
+    void Update()
+    {
+        if (currentGameObject == null)
+        {
+            currentGameObject = Instantiate(prefabMesh, new Vector3(2.6f,hideForGenarationMesh,15f), Quaternion.identity);
+            movingFlag = true;
+            hideForGenarationMesh += 0.7f;
+        }
+
+        if (currentGameObject != null && movingFlag)
+        {
+            currentGameObject.transform.position = currentGameObject.transform.position - Vector3.forward*Time.deltaTime*speed;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movingFlag = false;
+            currentGameObject.AddComponent<Rigidbody>();
+        }
+    }
+    
+    Vector3[] GenerateVertices(float coordX, float coordZ)
+    {
+        return new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),
+            new Vector3(0f, 0f, coordZ),
+            new Vector3(coordX, 0f, 0f),
+            new Vector3(coordX, 0f, coordZ)
+        };
+    }
+
+    int[] GenerateTriangle()
+    {
+        return new int[] {0, 1, 2, 1, 3, 2};
+    }
+}
---
+++ homework2_1_v1/Assets/MeshGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshGenerator : MonoBehaviour
+{
+    private float meshSize = 10f;
+    private float basedMeshSize = 10f;
+
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = GenerateVertices(meshSize, meshSize);
+        mesh.triangles = GenerateTriangle();
+        mesh.RecalculateNormals();
+        MeshCollider collider = gameObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float centerOfBasedMesh = basedMeshSize / 4f;
+        if (gameObject.transform.position.x > centerOfBasedMesh && gameObject.transform.position.z > 0f)
+        {
+            GenerateFallingMesh(
+                GenerateVertices(basedMeshSize - gameObject.transform.position.x, basedMeshSize - gameObject.transform.position.z), gameObject.transform.position);
+            GenerateFallingMesh(
+                GenerateVertices(meshSize - (basedMeshSize - gameObject.transform.position.x), basedMeshSize - gameObject.transform.position.z),
+                new Vector3(gameObject.transform.position.x + (basedMeshSize - gameObject.transform.position.x), gameObject.transform.position.y, gameObject.transform.position.z));
+            GenerateFallingMesh(
+                GenerateVertices(basedMeshSize - gameObject.transform.position.x, meshSize - (basedMeshSize - gameObject.transform.position.z)),
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + (basedMeshSize - gameObject.transform.position.z)));
+            GenerateFallingMesh(
+                GenerateVertices(meshSize - (basedMeshSize - gameObject.transform.position.x), meshSize - (basedMeshSize - gameObject.transform.position.z)),
+                new Vector3(gameObject.transform.position.x + (basedMeshSize - gameObject.transform.position.x), gameObject.transform.position.y, gameObject.transform.position.z + (basedMeshSize - gameObject.transform.position.z)));
+
+            Destroy(gameObject);
+        }
+
+        if (gameObject.transform.position.x > centerOfBasedMesh && gameObject.transform.position.z < 0f)
+        {
+            GenerateFallingMesh(
+                GenerateVertices(basedMeshSize - gameObject.transform.position.x, meshSize - Math.Abs(gameObject.transform.position.z)),
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0f));
+            GenerateFallingMesh(
+                GenerateVertices(meshSize - (basedMeshSize - gameObject.transform.position.x), meshSize - Math.Abs(gameObject.transform.position.z)),
+                new Vector3(gameObject.transform.position.x + (basedMeshSize - gameObject.transform.position.x), gameObject.transform.position.y, 0f));
+            GenerateFallingMesh(
+                GenerateVertices(basedMeshSize - gameObject.transform.position.x, Math.Abs(gameObject.transform.position.z)),
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -Math.Abs(gameObject.transform.position.z)));
+            GenerateFallingMesh(
+                GenerateVertices(meshSize - (basedMeshSize - gameObject.transform.position.x), meshSize - Math.Abs(gameObject.transform.position.z)), 
+                new Vector3(gameObject.transform.position.x + (basedMeshSize - gameObject.transform.position.x), gameObject.transform.position.y, -Math.Abs(gameObject.transform.position.z)));
+            
+            Destroy(gameObject);
+        }
+
+    }
+    
+    Vector3[] GenerateVertices(float coordX, float coordZ)
+    {
+        return new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),
+            new Vector3(0f, 0f, coordZ),
+            new Vector3(coordX, 0f, 0f),
+            new Vector3(coordX, 0f, coordZ)
+        };
+    }
+    
+    int[] GenerateTriangle()
+    {
+        return new int[] {0, 1, 2, 1, 3, 2};
+    }
+
+    void GenerateFallingMesh(Vector3[] vertices, Vector3 position)
+        {
+            GameObject tempCurrentMesh = new GameObject();
+            tempCurrentMesh.AddComponent<MeshFilter>();
+            tempCurrentMesh.AddComponent<MeshRenderer>();
+            Mesh tempMesh = new Mesh();
+            tempCurrentMesh.GetComponent<MeshFilter>().mesh = tempMesh;
+            tempMesh.vertices = vertices;
+            tempMesh.triangles = GenerateTriangle();
+            tempMesh.RecalculateNormals();
+            MeshCollider collider = tempCurrentMesh.AddComponent<MeshCollider>();
+            collider.convex = true;
+            tempCurrentMesh.GetComponent<MeshRenderer>().material.color = Color.green;
+            tempCurrentMesh.transform.position = position;
+            tempCurrentMesh.AddComponent<Rigidbody>();
+        }
+}
+
+
