0c41d0aa51a1d685b75a24d1b38f447eb5cf4154
LGavrik <lena.gavrilova2410@gmail.com>
2021-04-26 19:20:21 +0300

    [2-1] - cube

--- homework2_1_v1/Assets/MeshGenerator.cs
+++ homework2_1_v1/Assets/MeshGenerator.cs
@@ -66,13 +66,25 @@ public class MeshGenerator : MonoBehaviour
             new Vector3(0f, 0f, 0f),
             new Vector3(0f, 0f, coordZ),
             new Vector3(coordX, 0f, 0f),
-            new Vector3(coordX, 0f, coordZ)
+            new Vector3(coordX, 0f, coordZ),
+            
+			new Vector3(0f, 1f, 0f),
+            new Vector3(coordX, 1f, 0f), 
+            
+            new Vector3(0f, 1f, coordZ),
+            new Vector3(coordX, 1f, coordZ)
         };
     }
     
     int[] GenerateTriangle()
     {
-        return new int[] {0, 1, 2, 1, 3, 2};
+        return new int[] {0, 1, 2, 1, 3, 2, 
+            4, 2, 0, 5, 2, 4,
+            3, 7, 1, 7, 6, 1,
+            3, 2, 7, 5, 7, 2,
+            4, 6, 5, 6, 7, 5
+            
+        };
     }
 
     void GenerateFallingMesh(Vector3[] vertices, Vector3 position)
